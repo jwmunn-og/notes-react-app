360d0d8cff6241d1ad7571ca30eee8d0ffa984f6
jwmunn <contact@justinmunn.co>
2017-11-04 13:47:14 -0700

    Toggle state to show List or Note.

--- src/App.js
+++ src/App.js
@@ -12,12 +12,18 @@ class App extends Component {
     };
   }
 
+  toggleNote = () => {
+    this.setState({
+      showNote: ! this.state.showNote
+    })
+  }
+
   render() {
     const { showNote } = this.state;
 
     return (
       <div className="App">
-        <Nav />
+        <Nav toggleNote={this.toggleNote} showNote={showNote} />
         { showNote ? <Note /> : <List /> }
       </div>
     );
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 class Nav extends React.Component {
   render() {
+    const { toggleNote, showNote } = this.props;
+    
     return (
       <div className="nav-container">
         <div className="nav-logo">Notes</div>
-        <div className="nav-button">
-          + Note
+        <div className="nav-button" onClick={() => toggleNote()} >
+          { showNote ? 'Cancel' :  '+ Note' }
         </div> 
       </div>
     );
