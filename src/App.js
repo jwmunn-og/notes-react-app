d242f8be3b8415b4eb6d6fd82b9df5baf77969da
jwmunn <contact@justinmunn.co>
2017-11-05 15:28:51 -0800

    Sync note submission and list to API.

--- src/App.js
+++ src/App.js
@@ -35,7 +35,9 @@ class App extends Component {
   }
 
   submitNote = (data) => {
-    console.log(data);
+    axios.post(urlFor('notes'), data)
+    .then((res) => this.setState({ showNote: false }) )
+    .catch((err) => console.log(err.response.data) );
   }
 
   render() {
