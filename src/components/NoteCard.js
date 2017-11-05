88653ae6729f70ca6e56f0e7e4fa079a2321863f
jwmunn <contact@justinmunn.co>
2017-11-05 13:11:07 -0800

    Add material icons to notecards.

--- src/components/NoteCard.js
+++ src/components/NoteCard.js
@@ -2,9 +2,21 @@ import React, { Component } from 'react';
 
 class NoteCard extends Component {
   render() {
+    const { note } = this.props;
     return(
       <div className="note-card-container">
-        Notecard Component
+        <div className="note-card-title">
+          {note.title}
+        </div>
+        <div className="note-card-content">
+          {note.content}
+        </div>
+        <span className="note-card-delete">
+          <i className="material-icons">close</i>
+        </span>
+        <span className="note-card-edit">
+          <i className="material-icons">mode_edit</i>
+        </span>
       </div>
     );
   }
