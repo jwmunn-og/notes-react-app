abdaa9232851da01c1f154465a493ec91ee6c513
jwmunn <contact@justinmunn.co>
2017-11-04 22:00:03 -0700

    Moved notes to the state.

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      showNote: false
+      showNote: false,
+      notes: []
     };
   }
 
@@ -22,17 +23,22 @@ class App extends Component {
 
   getNotes = () => {
     axios.get(urlFor('notes'))
-    .then((res) => console.log(res.data) )
+    .then((res) => this.setState({notes: res.data}) )
     .catch((err) => console.log(err.response.data) );
   }
 
   render() {
-    const { showNote } = this.state;
+    const { showNote, notes } = this.state;
 
     return (
       <div className="App">
         <Nav toggleNote={this.toggleNote} showNote={showNote} />
-        { showNote ? <Note /> : <List getNotes={this.getNotes} /> }
+        { showNote ?
+            <Note />
+            :
+            <List 
+              getNotes={this.getNotes}
+              note={notes} /> }
       </div>
     );
   }
